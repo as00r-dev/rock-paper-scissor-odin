a44ac3987778d0b8dcb71178e65e73549463666a
as00r <as00r.dev@gmail.com>
2022-11-08 18:38:53 +0530

    Add Game logic & Style basic UI
    Added game logic such that, game restarts when either's score is 5.
    game() handles main game logic. resetScore() resets score when score
    of either player or computer is 5.
    
    To do - Make better UI, Show computer choice, popup to restart game
    when score is 5 in place of automatic restart.

--- game.js
+++ game.js
@@ -17,33 +17,64 @@ function playRound(playerSelection, computerSelection) {
 	)}`;
 
 	if (playerSelection === "Rock" && computerSelection === "Paper") {
-		return "You Lose! Paper beats Rock.";
+		return 0;
 	} else if (playerSelection === "Rock" && computerSelection === "Scissor") {
-		return "Win! Rock beats Scissor.";
+		return 1;
 	} else if (playerSelection === "Rock" && computerSelection === "Rock") {
-		return "Its a Draw.";
+		return 2;
 	} else if (playerSelection === "Paper" && computerSelection === "Paper") {
-		return "Its a Draw.";
+		return 2;
 	} else if (playerSelection === "Paper" && computerSelection === "Scissor") {
-		return "You Lose! Scissor beats Paper.";
+		return 0;
 	} else if (playerSelection === "Paper" && computerSelection === "Rock") {
-		return "You Win! Paper beats Scissor.";
+		return 1;
 	} else if (playerSelection === "Scissor" && computerSelection === "Paper") {
-		return "You Win! Scissor beats Rock.";
+		return 1;
 	} else if (playerSelection === "Scissor" && computerSelection === "Scissor") {
-		return "Its a Draw.";
+		return 2;
 	} else if (playerSelection === "Scissor" && computerSelection === "Rock") {
-		return "You Lose! Rock beats Scissor.";
+		return 0;
 	}
 }
 
 //main game logic
-function game() {}
-
-//clickHandler function definition
-function clickHandler(e) {
-	RESULTS.textContent = playRound(e.target.textContent, getComputerChoice());
+function game(playerChoice, computerChoice) {
+	const resultCode = playRound(playerChoice, computerChoice);
+	if (resultCode === 1) {
+		playerScore++;
+		PLAYER_SCORE.textContent = playerScore;
+		RESULTS.textContent = "Round Won!";
+	} else if (resultCode === 2) {
+		computerScore++;
+		COMPUTER_SCORE.textContent = computerScore;
+		RESULTS.textContent = "Round Lost";
+	}
+	if (playerScore === 5 || computerScore === 5) {
+		if (playerScore > computerScore) {
+			RESULTS.textContent = "You Won!";
+		} else {
+			RESULTS.textContent = "You Lost!";
+		}
+		resetScore();
+	}
 }
+
+//Reset score
+function resetScore() {
+	playerScore = 0;
+	computerScore = 0;
+	PLAYER_SCORE.textContent = playerScore;
+	COMPUTER_SCORE.textContent = computerScore;
+}
+
+//Click Handler
+function clickHandler(e) {
+	game(e.target.textContent, getComputerChoice());
+}
+
+//Global game score counter
+let playerScore = 0;
+let computerScore = 0;
 
 //Create Elements
 const ROCK_BUTTON = document.createElement("button");
@@ -51,16 +82,27 @@ const PAPER_BUTTON = document.createElement("button");
 const SCISSOR_BUTTON = document.createElement("button");
 const RESULTS = document.createElement("div");
 const BUTTONS_WRAPPER = document.createElement("div");
+const GAME_START = document.createElement("button");
+const SCORE_WRAPPER = document.createElement("div");
+const PLAYER_SCORE = document.createElement("div");
+const COMPUTER_SCORE = document.createElement("div");
 
 //Select Elements
 const WRAPPER = document.querySelector(".wrapper");
+
+//For SCORE
+SCORE_WRAPPER.classList.add("score");
+SCORE_WRAPPER.appendChild(PLAYER_SCORE);
+SCORE_WRAPPER.appendChild(COMPUTER_SCORE);
+PLAYER_SCORE.classList.add("player");
+COMPUTER_SCORE.classList.add("computer");
+PLAYER_SCORE.textContent = 0;
+COMPUTER_SCORE.textContent = 0;
 
 //For BUTTONS
 ROCK_BUTTON.textContent = "Rock";
 PAPER_BUTTON.textContent = "Paper";
 SCISSOR_BUTTON.textContent = "Scissor";
-
-//For BUTTONS_WRAPPER
 BUTTONS_WRAPPER.classList.add("button-wrapper");
 BUTTONS_WRAPPER.appendChild(ROCK_BUTTON);
 BUTTONS_WRAPPER.appendChild(PAPER_BUTTON);
@@ -69,7 +111,8 @@ BUTTONS_WRAPPER.appendChild(SCISSOR_BUTTON);
 //For RESULT
 RESULTS.classList.add("result");
 
-//For WRAPPER
+//Append to WRAPPER
+WRAPPER.appendChild(SCORE_WRAPPER);
 WRAPPER.appendChild(BUTTONS_WRAPPER);
 WRAPPER.appendChild(RESULTS);
 
